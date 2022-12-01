acbd646114e57a4e7136962a83b8721681f3ecb9
Karizali <96954751+Karizali@users.noreply.github.com>
2022-12-01 21:20:38 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+// shape
+
+
+// function shape(ka) {
+//     var a = document.getElementById("red");
+    
+//     a.className=ka
+//   }
+
+
+//   shape end
+
+
+
+// paragraph
+
+
+// function colorchange(){
+//     var pa = document.getElementById('para');
+// var pgraph= pa.getElementsByTagName('P');
+// pa.className='colorpara'
+// console.log(pgraph);
+// }
+
+
+
+var pa = document.getElementById('para');
+var pgraph= pa.getElementsByTagName('P');
+
+
+
+// paragraph end
